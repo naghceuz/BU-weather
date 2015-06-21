193a74fbd7981a79a6fbda554da51911d27b2758
naghceuz <guanchenzhang@gmail.com>
2015-06-21 00:06:20 -0400

    now the app is able to get the data and show on the screen

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 angular.module('myApp',[])
-	.controller('MainController', function($scope, $timeout){
+	.controller('MainController', function($scope, $timeout, Weather){
+
+		
+
+		Weather.getWeatherForecast("CA/San_Francisco")
+		.then(function(data){
+			$scope.weather.forecast = data;
+		});
 
 		$scope.date = {};
 
@@ -8,34 +15,51 @@ angular.module('myApp',[])
 			$timeout(updateTime, 1000);
 		}
 
+		$scope.weather = {}
 		updateTime();
 
 	} )
 
-
 // use service
 // because ersists across controllers for the 
 // duration of the application’s lifetime and is the appropriate place for us to hide business logic away from the controller.
-angular.module('myApp', [])
+
 	.provider('Weather', function(){
 
-		var apiKey = "e9a44beac5a80b8d";
-
-		this.getUrl = function(type, ext){
-			return "http://api.wunderground.com/api/" +
-				this.apiKey + '/' + type + '/q' + ext 
-				+ '.json';
-		};
+		var apiKey = "";
 
 		this.setApiKey = function(key){
 			if(key) this.apiKey = key;
 		};
 
-		this.$get = function($http){
+		this.getUrl = function(type, ext){
+			return "http://api.wunderground.com/api/" +
+				this.apiKey + '/' + type + '/q/' + 
+				ext + '.json';
+		};
+
+		
+
+		this.$get = function($q, $http){
+			var self = this;
 			return{
-				//service object
-
-
+				getWeatherForecast: function(city){
+					var d = $q.defer();
+					$http(
+					{
+						method: 'GET',
+						url: self.getUrl('forecast',city),
+						cache:true
+					}).success(function(data){
+						// The wunderground API returns the
+						// object that nests the forecasts inside 
+						// the forecast.simpleforecast key
+						d.resolve(data.forecast.simpleforecast);
+					}).error(function(err){
+						d.reject(err);
+					});
+					return d.promise;
+				}
 			};
 		}
 
