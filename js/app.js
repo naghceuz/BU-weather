c20b005b5a7d74d3caad874f20533a07b3ab710d
naghceuz <guanchenzhang@gmail.com>
2015-06-20 23:33:43 -0400

    set up the service and API key (anyone can easily get the API key, therefore even this is very unprofessional I will leave there)

--- js/app.js
+++ js/app.js
@@ -11,3 +11,55 @@ angular.module('myApp',[])
 		updateTime();
 
 	} )
+
+
+// use service
+// because ersists across controllers for the 
+// duration of the application’s lifetime and is the appropriate place for us to hide business logic away from the controller.
+angular.module('myApp', [])
+	.provider('Weather', function(){
+
+		var apiKey = "e9a44beac5a80b8d";
+
+		this.getUrl = function(type, ext){
+			return "http://api.wunderground.com/api/" +
+				this.apiKey + '/' + type + '/q' + ext 
+				+ '.json';
+		};
+
+		this.setApiKey = function(key){
+			if(key) this.apiKey = key;
+		};
+
+		this.$get = function($http){
+			return{
+				//service object
+
+
+			};
+		}
+
+	})
+
+	.config(function(WeatherProvider){
+		WeatherProvider.setApiKey('e9a44beac5a80b8d');
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
